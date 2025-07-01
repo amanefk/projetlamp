956ec3f8df7d2c973c79a7c69bb53f847cb6695a
amanefk <158782549+amanefk@users.noreply.github.com>
2025-07-01 12:37:52 +0200

    Update bdd.php

--- TP Réseau/bdd/bdd.php
+++ TP Réseau/bdd/bdd.php
@@ -2,8 +2,8 @@
 
 try{
 
-	$user = "root";
-	$pass = "root";
+	$user = "user";
+	$pass = "xXmGSJud0EJmbC0s";
 	$bdd = new PDO('mysql:host=localhost;dbname=commentaires', $user, $pass);
 
 }catch(PDOException $e){
