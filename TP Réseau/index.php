53d6c4b02189048b6b23380132bde661600b802c
amanefk <158782549+amanefk@users.noreply.github.com>
2025-07-01 11:14:49 +0200

    Add files via upload

---
+++ TP Réseau/index.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+include('view/commun/header.php');
+
+$page = isset($_GET['page']) ? $_GET['page'] : 'accueil' ;
+
+switch ($page) {
+    case 'accueil':
+            include('view/accueil.php');
+        break;
+    
+    default:
+        include('view/accueil.php');
+        break;
+}
+include('view/commun/footer.php');
+?>
---
+++ TP Réseau/view/commun/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Commentaires avec Étoiles</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+        <style>
+            .etoiles { color: gold; }
+        </style>
+    </head>
+<body>
+    <center>
