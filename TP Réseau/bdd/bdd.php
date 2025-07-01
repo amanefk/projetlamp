a1555890946f201eca3701f04e52b3555e033232
amanefk <158782549+amanefk@users.noreply.github.com>
2025-07-01 12:59:12 +0200

    Update bdd.php

--- TP Réseau/bdd/bdd.php
+++ TP Réseau/bdd/bdd.php
@@ -4,7 +4,7 @@ try{
 
 	$user = "user";
 	$pass = "xXmGSJud0EJmbC0s";
-	$bdd = new PDO('mysql:host=localhost;dbname=commentaires', $user, $pass);
+	$bdd = new PDO('mysql:host=localhost;dbname=lampbdd', $user, $pass);
 
 }catch(PDOException $e){
 	print "Erreur! :" .$e->getMessage() .
